4d33d10d5bbfbbf5f03df910c4535c624f458ee1
Yasith Fernando <thekindofme@gmail.com>
2014-01-29 17:13:12 +0800

    Initiallize a new Rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IndeedScraper::Application.config.secret_key_base = '20bb5a747e7e838dccbd8312826a917548852ceccc00321eec5a01e86aaba68ed1f01d2561f837d4e40590ad385f5801f8fb3f5fd4fe06502fc0aceb0dce21b0'
