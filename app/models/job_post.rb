685b54c01f3514d961a5f660941ffc30f6a815b4
Yasith Fernando <thekindofme@gmail.com>
2014-02-21 17:55:27 +0800

    Job Post populator

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -10,4 +10,12 @@ class JobPost
   field :days_ago
   field :source
   field :country_code
+
+  def self.find_or_create_by_job_id job_id, hash
+    unless (r=JobPost.where(job_id:job_id)).count > 0
+      jp=JobPost.create(hash)
+    end
+
+    jp || r.first
+  end
 end
---
+++ lib/populator.rb
@@ -0,0 +1,8 @@
+class Populator
+ def indeed key_word='ruby', pages=10, country_code='ca'
+  results=Scrapers::Indeed.new(country_code).scrape(key_word, pages)
+  results.each do |result|
+    JobPost.find_or_create_by_job_id result[:job_id], result
+  end
+ end
+end
